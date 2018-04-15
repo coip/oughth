4596e49a4cf71f2674828694b9005abc74a3871c
ethan <e@coip.me>
2018-04-14 22:01:44 -0500

    added forms for login/register

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express 	= require('express');
 const app	= express();
 
+const bodyParser = require('body-parser');
+const util	= require('util');
+
 const speakeasy = require('speakeasy');
 const qrcode	= require('qrcode');
 
 const MongoClient = require('mongodb').MongoClient
+
 let db;
 
 const config = {
@@ -14,13 +18,29 @@ const config = {
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({ extended : true }));
+
+app.use((req, res, next) => { console.log("%s request on %s", req.method, req.path); next(); });
+
+app.get('/login', (req, res) => res.render('login'));
+app.post('/login', (req, res) => {
+	console.log("%s just signed in!", req.body.username);
+	res.send(req.body);
+});
+
+app.get('/register', (req, res) => res.render('register'));
+app.post('/register', (req, res) => {
+	console.log("%s just signed up!", req.body.username);
+	res.send(req.body);
+});
+
 app.get('/home', (req, res) => res.render('home'));
 
 MongoClient.connect(config['db-url'], (err, client) => {
   if (err) return console.log(err)
   db = client.db('otp-demo') // whatever your database name is
   app.listen(3000, () => {
-    console.log('listening on 3000')
+    console.log('listening on 3000');
   })
 })
 
